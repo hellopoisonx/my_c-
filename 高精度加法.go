0eaffb7ac9ba4c2a16d49fc32b8ba857e615a2f8
hellopoisonx <105351663+hellopoisonx@users.noreply.github.com>
2023-05-18 07:56:56 +0000

    temp file

---
+++ 高精度加法.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+func Max(a uint, )
+func reverse_str(a string) []byte {
+	var ans []byte
+	for i := 0; i < len(a); i ++ {
+		for j := len(a) - 1; j > i ; j -- {
+			ans[i], ans[j] = a[j], a[i]
+		}
+	}
+	return 
+}
+func high_plus(a string, b string) string {
+	_a := reverse_str(a)
+	_b := reverse_str(b)
+	var ans []byte
+	var next int = 0
+	for i := 0; i < len()
+}
+func main() {
+
+}
